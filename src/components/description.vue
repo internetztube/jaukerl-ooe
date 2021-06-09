73390da5745262eff11cfcff89966f63f774a597
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2021-06-09 21:09:20 +0200

    Add description for second shot.

--- src/components/description.vue
+++ src/components/description.vue
@@ -1,23 +1,36 @@
 <template>
   <div>
-    <p>
-      Einen Termin kannst du hier buchen: <br>
-      <a href="https://www.land-oberoesterreich.gv.at/files/covid19impfungopen/#/online-terminvereinbarung"
-         target="_blank" style="display: block;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">
-        https://www.land-oberoesterreich.gv.at/files/covid19impfungopen/#/online-terminvereinbarung
-      </a>
-    </p>
-    <p>
-      <b>Wenn du schon einen Termin hast,</b> registriere dich mit den selben Daten einfach nochmal. Wenn du einen
-      besseren Termin gefunden hast, den alten absagen und danach direkt den neuen Termin buchen.
-      <br>Absagen kannst du den Termin mit dem Link aus der E-Mail deiner alten Buchung. Solange du den alten Termin
-      nicht abgesagt hast, kannst du auch keinen neuen Termin buchen.
-      <br>Alle Angaben ohne Gewähr.
-      <br>
-      <br>
-      Weitere Informationen gibt es auf der Website des Landes Oberösterreich:
-      <br>
-      <a href="https://www.land-oberoesterreich.gv.at/ooe-impft.htm">http://ooe-impft.at/</a>
-    </p>
+    Einen Termin für deine Erstimpfung kannst du hier buchen: <br>
+    <a href="https://www.land-oberoesterreich.gv.at/files/covid19impfungopen/#/online-terminvereinbarung"
+       target="_blank" style="display: block;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">
+      https://www.land-oberoesterreich.gv.at/files/covid19impfungopen/#/online-terminvereinbarung
+    </a>
+    <div class="row">
+      <div class="col-md-6">
+        <br>
+        <b>Wenn du schon einen Termin hast,</b><br> registriere dich mit den selben Daten einfach nochmal. Wenn du einen
+        besseren Termin gefunden hast, den alten absagen und danach direkt den neuen Termin buchen. Absagen kannst du
+        den Termin mit dem Link aus der E-Mail deiner alten Buchung. Solange du den alten Termin
+        nicht abgesagt hast, kannst du auch keinen neuen Termin buchen.
+      </div>
+      <div class="col-md-6">
+        <br>
+        <b>Zweitimpftermin</b><br>
+        Die Termine auf dieser Seite <b><u>beziehen sich ausschließlich auf die Erstimpfung.</u></b>
+        Den Termin zur erforderlichen ZWEITimpfung wird bei der Erstimpfung direkt vor Ort vereinbart.
+        Deine ZWEITimpfung kannst du hier verschieben:
+        <a href="https://www.land-oberoesterreich.gv.at/corona-impftermin.htm"
+           target="_blank" style="display: block;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">
+          https://www.land-oberoesterreich.gv.at/corona-impftermin.htm
+        </a>
+      </div>
+    </div>
+
+    <br>Alle Angaben ohne Gewähr.
+    <br>
+    <br>
+    Weitere Informationen gibt es auf der Website des Landes Oberösterreich:
+    <br>
+    <a href="https://www.land-oberoesterreich.gv.at/ooe-impft.htm">http://ooe-impft.at/</a>
   </div>
 </template>
